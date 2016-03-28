516aa82adfc47b0fced196361e04b888cd641607
DJ Roychoudhury and Ryan Carlile <dj@>
2016-03-28 13:56:17 -0700

    modified script so that it is not redundant.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,44 @@
 
 $(document).ready(function() {
   $("#things form").submit(function(event) {
-    event.preventDefault();
+    var things = ["thing1","thing2","thing3","thing4","thing5"];
 
-    var thing1 = $("input#thing1").val();
-    var thing2 = $("input#thing2").val();
-    var thing3 = $("input#thing3").val();
-    var thing4 = $("input#thing4").val();
-    var thing5 = $("input#thing5").val();
-    var things = [thing1,thing2,thing3,thing4,thing5];
-
-    var things2 = things;
-    things2.pop();
-    things2.pop();
-    things2.pop();
-    things2.pop();
-    things2.pop();
-    things2.push(thing2,thing1,thing3);
+      things.forEach(function(thing) {
+        var userInput = $("input#" + thing).val();
+        $("#" + thing).text(userInput);
+        console.log(userInput)
+        $("#result").append("<li>" + userInput + "</li>");
+      });
 
 
-
-    $("#result").text(things2);
-
+      event.preventDefault();
+    });
   });
-});
+
+
+
+// $(document).ready(function() {
+//   $("#things form").submit(function(event) {
+//     event.preventDefault();
+//
+//     var thing1 = $("input#thing1").val();
+//     var thing2 = $("input#thing2").val();
+//     var thing3 = $("input#thing3").val();
+//     var thing4 = $("input#thing4").val();
+//     var thing5 = $("input#thing5").val();
+//     var things = [thing1,thing2,thing3,thing4,thing5];
+//
+//     var things2 = things.slice();
+//     things2.pop();
+//     things2.pop();
+//     things2.pop();
+//     things2.pop();
+//     things2.pop();
+//     things2.push(thing2,thing1,thing3);
+//
+//
+//
+//     $("#result").text(things);
+//
+//   });
+// });
